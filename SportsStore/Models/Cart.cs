1cf750c9c4a4019da8e63b1f1986e507287ae90f
Jaewon Park <raph.ej.p@outlook.com>
2020-12-07 00:21:12 -0500

    Implemented 13% HST to CartClass ComputeTotalValue

--- SportsStore/Models/Cart.cs
+++ SportsStore/Models/Cart.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SportsStore.Models {
@@ -25,8 +26,16 @@ namespace SportsStore.Models {
         public virtual void RemoveLine(Product product) =>
             Lines.RemoveAll(l => l.Product.ProductID == product.ProductID);
 
-        public decimal ComputeTotalValue() =>
-            Lines.Sum(e => e.Product.Price * e.Quantity);
+        public decimal ComputeTotalValue()
+        {
+            decimal HST = 1.13m;
+            decimal cart_sum = Lines.Sum(e => e.Product.Price * e.Quantity);
+            cart_sum = cart_sum * HST;
+
+            return Math.Round(cart_sum, 2);
+
+        }
+  
 
         public virtual void Clear() => Lines.Clear();
     }
