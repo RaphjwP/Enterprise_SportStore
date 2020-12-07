6d16da120d1d2b747a8a5633df6b9c8f12a9cbfb
Mike Komor <komormi@shernet.sheridancollege.ca>
2020-12-07 13:46:49 -0500

    Q3 cart summary - Mike

---
+++ SportsStore/Models/ViewModels/OrderListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using SportsStore.Models;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class OrderListViewModel
+    {
+        public Order Orders { get; set; }
+
+        public int TotalProduct { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
